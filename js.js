bf7e5184916393c03194fd6274d01e8dc4b1e696
Eduardogmaas <127263714+Eduardogmaas@users.noreply.github.com>
2023-09-05 21:46:55 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+const frm = document.createElement("label");
+const frm = document.querySelector("form");
+const dvQuadro = document.querySelector("#divQuadro");
+
+const 
+
+frm.addEvenListener("submit", (e) =>{
+    e.preventDefault();
+
+    const tarefa = frm.inNumero.value;
+    const h5 = document.createElement("h5");
+});
